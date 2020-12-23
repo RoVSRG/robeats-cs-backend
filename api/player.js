58c189d372439239b5568fafc275af972e7f3c46
thenerdie <stirmanjr@gmail.coom>
2020-12-23 14:57:12 -0500

    Fix null handling

--- api/player.js
+++ api/player.js
@@ -5,6 +5,9 @@ class PlayerAPI {
         const query = Profile.findOne({"UserId": userID});
         query.lean();
         const results = await query.exec();
+        if (results == null) {
+            return 0
+        }
         return await Profile.countDocuments({ Rating: {"$gt": results.Rating} }) + 1;
     };
 }
