d59eebf59346368e44db3d4a76e6a3b0a9035623
astralkingdummy <cjansah11@gmail.com>
2022-08-07 23:17:34 -0400

    update in de xxxx.js lol

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ let connectOptions = {
 if (process.env.rcsdb?.match("127.0.0.1") == null && process.env.rcsdb?.match("localhost") == null) {
     connectOptions.ssl = true
     connectOptions.sslCA = "./ca-certificate.crt"
-}
+}   
 
 console.log(connectOptions)
 
@@ -27,10 +27,6 @@ mongoose.connect(process.env.rcsdb, connectOptions).catch(err => console.log(err
 
 fastify.get("/", (request, reply) => {
     reply.type('text/html').send("<img src=\"https://forklores.files.wordpress.com/2012/11/pinto-beans-and-cornbread-3.jpg?w=663&h=501\"/>")
-
-    // score.find({UserId: 526993347}).exec((err, results) => {
-    //     reply.send(results)
-    // })
 })
 
 fastify.register(require("./routes/scores"), { prefix: "/api/scores" })
@@ -39,9 +35,15 @@ fastify.register(require("./routes/bans"), { prefix: "/api/bans" })
 fastify.register(require("./routes/matchmaking"), { prefix: "/api/matchmaking" })
 fastify.register(require("./routes/difficulties"), { prefix: "/api/difficulties" })
 
-fastify.listen(process.env.rcsport ? Number.parseInt(process.env.rcsport) : 3000, function (err, address) {
-    if (err) {
-        fastify.log.error(err)
-        process.exit(1)
-    }
+// fastify.listen(process.env.port ? Number.parseInt(process.env.port) : 3000, function (err, address) {
+//     if (err) {
+//         fastify.log.error(err)
+//         process.exit(1)
+//     }
+// })
+
+fastify.listen({ port: process.env.port ? Number.parseInt(process.env.port) : 3000 }, (err, address) => {
+    if (!err) return;
+    fastify.log.error(err)
+    process.exit(1)
 })
